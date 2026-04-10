9691e70f64f94be12edb5c064a6619231d751bce
Esteban Bolaños Rodriguez <estebanbolp@gmail.com>
2026-04-10 18:43:37 -0500

    Add files via upload

---
+++ serve.ps1
@@ -0,0 +1,44 @@
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8090/")
+$listener.Start()
+Write-Host "Server running at http://localhost:8090/"
+
+$mimeTypes = @{
+    ".html" = "text/html"
+    ".css"  = "text/css"
+    ".js"   = "application/javascript"
+    ".png"  = "image/png"
+    ".jpg"  = "image/jpeg"
+    ".gif"  = "image/gif"
+    ".svg"  = "image/svg+xml"
+    ".ico"  = "image/x-icon"
+}
+
+$root = "d:\Antigravity\storybook"
+
+while ($listener.IsListening) {
+    $context = $listener.GetContext()
+    $request = $context.Request
+    $response = $context.Response
+
+    $localPath = $request.Url.LocalPath
+    if ($localPath -eq "/") { $localPath = "/index.html" }
+
+    $filePath = Join-Path $root ($localPath.TrimStart("/").Replace("/", "\"))
+
+    if (Test-Path $filePath) {
+        $ext = [System.IO.Path]::GetExtension($filePath)
+        $contentType = if ($mimeTypes.ContainsKey($ext)) { $mimeTypes[$ext] } else { "application/octet-stream" }
+        $response.ContentType = $contentType
+        $bytes = [System.IO.File]::ReadAllBytes($filePath)
+        $response.ContentLength64 = $bytes.Length
+        $response.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+        $response.StatusCode = 404
+        $msg = [System.Text.Encoding]::UTF8.GetBytes("Not Found")
+        $response.OutputStream.Write($msg, 0, $msg.Length)
+    }
+
+    $response.Close()
+    Write-Host "$($request.HttpMethod) $localPath -> $($response.StatusCode)"
+}
